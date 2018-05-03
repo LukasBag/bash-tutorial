a548c41ed14365e67ec55fbb6660053916cc927a
LukasBag <LukasB@centos7.0>
2018-05-03 11:26:40 +0300

    bash-tutorial

---
+++ first.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Comment
+echo  "Hello         World" # Comment 
+echo "Hello World"
+echo "Hello * World"
+echo Hello * World
+echo Hello      World
+echo "Hello" World
+echo Hello "     " World
+echo "Hello "*" World"
+echo `hello` world
+echo 'hello' world
